ecf6012da05df59694de145660287b4d66f660d6
Tamakuz <jovangithub@gmail.com>
2023-10-14 00:20:43 +0700

    first commit

--- db/connect.js
+++ db/connect.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    mongoose.connect("mongodb://127.0.0.1:27017/si-hd");
+    // mongoose.connect("mongodb://127.0.0.1:27017/si-hd");
+    mongoose.connect(
+      "mongodb+srv://jovangithub:jovangithub@personal.cl2jypz.mongodb.net/si-hd?retryWrites=true&w=majority"
+    );
     console.log("DB connected");
   } catch (error) {
     console.log("Connection failed");
